5240f0a6bcd6950cd22b3330fa074abe65c1491f
Jason Dobry <jason.dobry@gmail.com>
2013-08-26 23:11:51 -0600

    Finished multiline and spaced key parsing capabilities. #1
    Started on unit tests for parse function. #4

--- test/angular-i18n-propertiesSpec.js
+++ test/angular-i18n-propertiesSpec.js
@@ -1,8 +1,80 @@
 describe('angular-i18n-properties', function () {
 
-	var $timeout;
+	var $timeout, I18nService;
+
+	function getProperties(filename, cb) {
+		var req = new XMLHttpRequest(),
+			url = 'base/test/' + filename;
+		req.open("GET", url + ((/\?/).test(url) ? "&" : "?") + (new Date()).getTime(), false);
+
+		req.setRequestHeader('Content-Type', 'text/plain;charset=UTF-8');
+
+		req.onreadystatechange = function (event) {
+			var status, err;
+			if (req.readyState === 4) {
+				status = req.status;
+				if (status > 399 && status < 600) {
+					err = new Error(url + ' HTTP status: ' + status);
+					err.xhr = req;
+					err.event = event;
+					console.error(err);
+				} else {
+					cb(angular.extend({}, I18nService.parse(req.responseText)));
+				}
+			}
+		};
+		req.send(null);
+	}
+
 	beforeEach(module('jmdobry.angular-i18n-properties'));
-	beforeEach(inject(function ($injector) {
+	beforeEach(inject(function ($injector, _I18nService_) {
 		$timeout = $injector.get('$timeout');
+		I18nService = _I18nService_;
 	}));
+
+	describe('I18nService', function () {
+		describe('parse()', function () {
+			it('should correctly parse multiline.properties', function () {
+				getProperties('multiline.properties', function (properties) {
+					expect(properties).toEqual({
+						'oneline': '1 line',
+						'multiline': 'I have four lines.',
+						'multiline with  key spaces': 'line 1line 2'
+					});
+				});
+			});
+
+			it('should correctly parse key_delimiters.properties', function () {
+				getProperties('key_delimiters.properties', function (properties) {
+					expect(properties).toEqual({
+						'equal.no.space': 'no.space',
+						'equal.left.space': 'left.space',
+						'equal.right.space': 'right.space',
+						'equal.both.space': 'both.space',
+						'equal.left.tab': 'left.tab',
+						'equal.right.tab': 'right.tab',
+						'equal.both.tab': 'both.tab',
+						'equal.left.mixed': 'left.mixed',
+						'equal.right.mixed': 'right.mixed',
+						'equal.both.mixed': 'both.mixed',
+						'colon.no.space': 'no.space',
+						'colon.left.space': 'left.space',
+						'colon.right.space': 'right.space',
+						'colon.both.space': 'both.space',
+						'colon.left.tab': 'left.tab',
+						'colon.right.tab': 'right.tab',
+						'colon.both.tab': 'both.tab',
+						'colon.left.mixed': 'left.mixed',
+						'colon.right.mixed': 'right.mixed',
+						'colon.both.mixed': 'both.mixed',
+						'space': 'space',
+						'spaced key': 'spaced key',
+						'space.equal': 'equal = space',
+						'space.colon': 'colon : space',
+						'equal equal equal': 'value'
+					});
+				});
+			});
+		});
+	});
 });
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,73 +1,70 @@
-// Karma configuration
-// Generated on Tue May 21 2013 18:52:40 GMT-0600 (Mountain Daylight Time)
+// an example karma.conf.js
+module.exports = function (config) {
+	config.set({
+		// base path, that will be used to resolve files and exclude
+		basePath: '../',
+		frameworks: ['jasmine'],
+		plugins: [
+			// these plugins will be require() by Karma
+			'karma-jasmine',
+			'karma-chrome-launcher',
+			'karma-phantomjs-launcher',
+			'karma-coverage'
+		],
+		autoWatch: true,
+		// Start these browsers, currently available:
+		// - Chrome
+		// - ChromeCanary
+		// - Firefox
+		// - Opera
+		// - Safari (only Mac)
+		// - PhantomJS
+		// - IE (only Windows)
+		browsers: ['Chrome'],
 
-// base path, that will be used to resolve files and exclude
-basePath = '../';
+		// list of files / patterns to load in the browser
+		files: [
+			'test/vendor/angular-1.1.5.min.js',
+			'test/vendor/angular-mocks-1.1.5.js',
+			'src/angular-i18n-properties.js',
+			{ pattern: 'test/*.properties', included: false },
+			'test/angular-i18n-propertiesSpec.js'
+		],
 
-// list of files / patterns to load in the browser
-files = [
-    JASMINE,
-    JASMINE_ADAPTER,
-    'test/vendor/angular-1.1.5.min.js',
-    'test/vendor/angular-mocks-1.1.5.js',
-    'src/angular-i18n-properties.js',
-    'test/angular-i18n-propertiesSpec.js'
-];
+		exclude: [
+			'dist/'
+		],
 
-// list of files to exclude
-exclude = [
-    'dist/'
-];
+		// test results reporter to use
+		// possible values: dots || progress || growl
+		reporters: ['progress', 'coverage'],
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress', 'coverage'];
+		preprocessors: {
+			'src/angular-i18n-properties.js': ['coverage']
+		},
 
-preprocessors = {
-    'src/angular-i18n-properties.js': 'coverage'
+		coverageReporter: {
+			type: 'html',
+			dir: 'coverage/'
+		},
+
+		// web server port
+		port: 8000,
+
+		// cli runner port
+		runnerPort: 9100,
+
+		// enable / disable colors in the output (reporters and logs)
+		colors: true,
+
+		// level of logging
+		logLevel: config.LOG_INFO,
+
+		// If browser does not capture in given timeout [ms], kill it
+		captureTimeout: 30000,
+
+		// Continuous Integration mode
+		// if true, it capture browsers, run tests and exit
+		singleRun: false
+	});
 };
-
-coverageReporter = {
-    type: 'html',
-    dir: 'coverage/'
-};
-
-// web server port
-port = 9876;
-
-// cli runner port
-runnerPort = 9100;
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
-
-var os = require('os');
-if (os.platform() !== 'win32') {
-    process.env.PHANTOMJS_BIN = './node_modules/phantomjs/lib/phantom/bin/phantomjs';
-} else {
-    process.env.PHANTOMJS_BIN = './node_modules/phantomjs/lib/phantom/phantomjs.exe';
-}
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
