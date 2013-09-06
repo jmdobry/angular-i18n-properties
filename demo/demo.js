3a73c05bfbc4418d5c9935692c7a685f4ede76a9
Jason Dobry <jason.dobry@gmail.com>
2013-09-05 19:16:04 -0600

    Merge

--- demo/demo.js
+++ demo/demo.js
@@ -1,9 +1,16 @@
 var app = angular.module('app', ['jmdobry.angular-i18n-properties']);
 
 app.config(['I18nServiceProvider', function (I18nServiceProvider) {
-		I18nServiceProvider.config({});
-	}]).run(['I18nService', function (I18nService) {
-		I18nService.load('demo/messages.properties');
-		I18nService.load('../test/multiline.properties');
-		I18nService.load('../test/key_delimiters.properties');
-	}]);
+    I18nServiceProvider.config({});
+}]).run(['I18nService', function(I18nService) {
+    I18nService.load('messages.properties');
+	I18nService.load('../test/multiline.properties');
+
+	I18nService.load('../test/key_delimiters.properties', {
+		lang: 'kk_KK'
+	});
+}]);
+
+app.controller('DemoCtrl', function ($scope, I18nService, $filter) {
+
+});
